b1449e7033b0bffa7ab1530d1ece9c1db68b5ca3
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-05 22:48:17 -0300

    Criação da classe de tipos numericos

---
+++ src/CursoLogicaCapitulo03/TiposNumericos.java
@@ -0,0 +1,22 @@
+package CursoLogicaCapitulo03;
+
+public class TiposNumericos {
+
+	public static void main(String[] args) {
+		// Inteiros 
+		Byte numeroByte = 1; // Vai de -128 até 127
+
+		Short numeroShort = 1; // Vai de -32768 até 32767
+
+		Integer numeroInteger = 2147483647; // Vai de -2147483648 até 2147483647
+
+		Long numeroLong = 1L;
+
+		// Decimais
+		
+		Float numeroFloat = 1.0f;
+		
+		Double numeroDouble = 1.0;
+	}
+
+}
