91fe34bc84e47106f712dfcc9f657c382987e5c1
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-05 23:00:49 -0300

    Criaçao da classe de TipoTexto

---
+++ src/CursoLogicaCapitulo03/TipoTexto.java
@@ -0,0 +1,18 @@
+package CursoLogicaCapitulo03;
+
+import java.util.Scanner;
+
+public class TipoTexto {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String nome = scanner.nextLine();
+		
+		System.out.println("Olá " + nome + "!");
+		
+		scanner.close();
+
+	}
+
+}
