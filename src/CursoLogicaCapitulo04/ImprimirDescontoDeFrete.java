6098419ac99ed26bc930fae374886b83f84015a2
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-09-08 09:18:39 -0300

    Create ImprimirDescontoDeFrete.java

---
+++ src/CursoLogicaCapitulo04/ImprimirDescontoDeFrete.java
@@ -0,0 +1,25 @@
+package CursoLogicaCapitulo04;
+
+import java.util.Scanner;
+
+public class ImprimirDescontoDeFrete {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+
+		Boolean precisaCobrarFrete = valorProduto < 100;
+
+		Double valorFinal = valorProduto;
+
+		if (precisaCobrarFrete) {
+			valorFinal += 15.0;
+		}
+
+		System.out.println("Valor final: " + valorFinal);
+
+		scanner.close();
+
+	}
+}
